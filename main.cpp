3f8d66b8896c9f51a2e77077d81a5570a24e67a7
mdangga <108461444+mdangga@users.noreply.github.com>
2023-12-22 17:56:53 +0800

    Update main.cpp
    menambah fitur mengulang program

--- main.cpp
+++ main.cpp
@@ -4,40 +4,37 @@
 
 using namespace std;
 
-void rotateMatrixCounterClockwise(vector<vector<int>>& matrix) {
+void rotateMatrixCounterClockwise(vector<vector<int>>& matrix, int rows, int cols) {
     int n = matrix.size();
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < rows; ++i) {
         reverse(matrix[i].begin(), matrix[i].end());
     }
-
     // Transpose the matrix
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            swap(matrix[j][i], matrix[i][j]);
-        }
-    }
-}
-
-void rotateMatrixClockwise(vector<vector<int>>& matrix) {
-    int n = matrix.size();
-
-    // Transpose the matrix
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = i + 1; j < cols; ++j) {
             swap(matrix[j][i], matrix[i][j]);
         }
     }
 
-    for (int i = 0; i < n; ++i) {
+}
+void rotateMatrixClockwise(vector<vector<int>>& matrix, int rows, int cols) {
+    int n = matrix.size();
+
+    // Transpose the matrix
+    for (int i = 0; i < rows; ++i) {
+        for (int j = i + 1; j < cols; ++j) {
+            swap(matrix[j][i], matrix[i][j]);
+        }
+    }
+    for (int i = 0; i < rows; ++i) {
         reverse(matrix[i].begin(), matrix[i].end());
     }
+
 }
 
-void printMatrix(const vector<vector<int>>& matrix) {
-    int rows = matrix.size();
-    int cols = matrix[0].size();
-
+void printMatrix(const vector<vector<int>>& matrix, int rows, int cols) {
+    int n = matrix.size();
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
             cout << matrix[i][j] << " ";
@@ -45,16 +42,18 @@ void printMatrix(const vector<vector<int>>& matrix) {
         cout << endl;
     }
 }
-
-int main() {
-    vector<vector<int>> matrix;
+void fungsi1(){
+    //vector<vector<int>> matrix = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+    vector<vector<int>>matrix;
     int rows, cols;
 
     cout << "Masukkan jumlah baris matriks: ";
     cin >> rows;
+    cin.ignore();
 
     cout << "Masukkan jumlah kolom matriks: ";
     cin >> cols;
+    cin.ignore();
 
     // Mengalokasikan ruang untuk matriks
     matrix.resize(rows, vector<int>(cols));
@@ -65,27 +64,40 @@ int main() {
         for (int j = 0; j < cols; ++j) {
             cout << "Masukkan elemen matriks [" << i + 1 << "][" << j + 1 << "]: ";
             cin >> matrix[i][j];
+            cin.ignore();
         }
     }
 
     cout << "Matriks Awal:" << endl;
-    printMatrix(matrix);
+    printMatrix(matrix, rows, cols);
+     int b;
+    cout<<"masukkan angka: ";cin>>b;cin.ignore();// 1. searah jarum jam, 2. berlawanan jarum jam
 
-    int b;
-    cout << "Masukkan angka: ";
-    cin >> b; // 1. searah jarum jam, 2. berlawanan jarum jam
-
-    if (b == 1) {
-        rotateMatrixClockwise(matrix);
+    if(b == 1){
+        rotateMatrixClockwise(matrix, rows, cols);
         cout << "\nMatriks Setelah Diputar 90 Derajat Clockwise:" << endl;
-        printMatrix(matrix);
-    } else if (b == 2) {
-        rotateMatrixCounterClockwise(matrix);
+        printMatrix(matrix, rows, cols);
+    }else if(b == 2){
+        rotateMatrixCounterClockwise(matrix, rows, cols);
         cout << "\nMatriks Setelah Diputar 90 Derajat Counter-Clockwise:" << endl;
-        printMatrix(matrix);
-    } else {
-        cout << "Pilihan tidak ada" << endl;
+        printMatrix(matrix, rows, cols);
+    }else{
+        cout<<"pilihan tidak ada"<<endl;
     }
+}
 
-    return 0;
+int main() {
+    char per;
+
+    do {
+        fungsi1();
+
+        cout << "Apakah anda ingin mengulang? (y/n): ";
+        cin >> per;
+
+        if (per == 'y' || per == 'Y') {
+            system("cls");
+        }
+
+    } while (per == 'y' || per == 'Y');
 }
