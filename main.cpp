a166cd19b7d0e1351c686a8e21928fd858e5e23c
mdangga <108461444+mdangga@users.noreply.github.com>
2023-12-22 09:23:36 +0800

    Update main.cpp
    menambahkan fungsi untuk menerima input user

--- main.cpp
+++ main.cpp
@@ -10,14 +10,15 @@ void rotateMatrixCounterClockwise(vector<vector<int>>& matrix) {
     for (int i = 0; i < n; ++i) {
         reverse(matrix[i].begin(), matrix[i].end());
     }
+
     // Transpose the matrix
     for (int i = 0; i < n; ++i) {
         for (int j = i + 1; j < n; ++j) {
             swap(matrix[j][i], matrix[i][j]);
         }
     }
-
 }
+
 void rotateMatrixClockwise(vector<vector<int>>& matrix) {
     int n = matrix.size();
 
@@ -27,16 +28,18 @@ void rotateMatrixClockwise(vector<vector<int>>& matrix) {
             swap(matrix[j][i], matrix[i][j]);
         }
     }
+
     for (int i = 0; i < n; ++i) {
         reverse(matrix[i].begin(), matrix[i].end());
     }
-
 }
 
 void printMatrix(const vector<vector<int>>& matrix) {
-    int n = matrix.size();
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
@@ -44,25 +47,45 @@ void printMatrix(const vector<vector<int>>& matrix) {
 }
 
 int main() {
-    int b;
-    vector<vector<int>> matrix = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+    vector<vector<int>> matrix;
+    int rows, cols;
+
+    cout << "Masukkan jumlah baris matriks: ";
+    cin >> rows;
+
+    cout << "Masukkan jumlah kolom matriks: ";
+    cin >> cols;
+
+    // Mengalokasikan ruang untuk matriks
+    matrix.resize(rows, vector<int>(cols));
+
+    cout << "Masukkan elemen-elemen matriks:\n";
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << "Masukkan elemen matriks [" << i + 1 << "][" << j + 1 << "]: ";
+            cin >> matrix[i][j];
+        }
+    }
 
     cout << "Matriks Awal:" << endl;
     printMatrix(matrix);
-    cout<<"masukkan angka: ";cin>>b;// 1. searah jarum jam, 2. berlawanan jarum jam
 
-    if(b == 1){
+    int b;
+    cout << "Masukkan angka: ";
+    cin >> b; // 1. searah jarum jam, 2. berlawanan jarum jam
+
+    if (b == 1) {
         rotateMatrixClockwise(matrix);
         cout << "\nMatriks Setelah Diputar 90 Derajat Clockwise:" << endl;
         printMatrix(matrix);
-    }if else(b == 2){
+    } else if (b == 2) {
         rotateMatrixCounterClockwise(matrix);
         cout << "\nMatriks Setelah Diputar 90 Derajat Counter-Clockwise:" << endl;
         printMatrix(matrix);
-    }else{
-        cout<<"pilihan tidak ada"<<endl;
+    } else {
+        cout << "Pilihan tidak ada" << endl;
     }
-
 
     return 0;
 }
